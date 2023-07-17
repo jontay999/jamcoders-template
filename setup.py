fc8f1064b238b88b02061d8de2b928097da81053
jontay999 <45443638+jontay999@users.noreply.github.com>
2023-07-17 23:01:18 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jamcoders',
+    version='1.0.6',
+    author='Jamcoders',
+    author_email='jamcoders@jamcoders.com',
+    description='Utilities for JamCoders',
+    packages=find_packages(),
+    install_requires=[],
+)
